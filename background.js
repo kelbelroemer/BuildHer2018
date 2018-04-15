3f65e8e8f7c5cfb5528c2cea5c4ba299783a1108
Kelly Roemer <kellyroemer2019@u.northwestern.edu>
2018-04-14 20:23:51 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,45 +1,13 @@
 
-
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("The color is green.");
-	});
-
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'developer.chrome.com'},
-		})
-		],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if (/netflix\.com/.test(changeInfo.url)) {
+		console.log("tab updated, reloading script");
+		chrome.tabs.executeScript(null, { file: "javascript/contentscript.js" });
+	}
 });
-/*chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-	if (changeInfo.status == 'complete' && tab.active) {
- 		chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
- +			function(tabs){
- +				var url = new URL(tabs[0].url);
- +				var domain = url.hostname
- +
- +				var movie_zero = {
- +					type: "no_pass",
- +					title: "Source: " + domain,
- +					message: "This movie does not pass the Bechdel Test",
- +					iconUrl: "" //need icon for 
- +				}
- +				var movie_one = {
- +					type: "pass",
- +					title: "Source: " + domain,
- +					message: "This movie passes the Bechdel Test"
- +					iconUrl: "g" //need icon for pass
-}
- +				var movie_dict = {
- +					//'www.breitbart.com': rating_two,
- +					//'www.infowars.com': rating_three,
- +					//'www.apnews.com': rating_zero
- +				}
- +				chrome.notifications.create(movie_dict[domain]);
- +			});
- +	}
- +}) */
+
+chrome.runtime.onMessage.addListener(function(message, sender, response) {
+	if (message.type == 'showPageAction') {
+		chrome.pageAction.show(sender.tab.id);
+	}
+});
